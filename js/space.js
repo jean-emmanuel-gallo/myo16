929e5223e103bfa22826adc2c2aa6bb746196774
jean-emmanuel-gallo <115216537+jean-emmanuel-gallo@users.noreply.github.com>
2024-02-15 20:44:59 +0100

    Add files via upload

---
+++ js/space.js
@@ -0,0 +1,113 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var arrow = document.querySelector(".arrow-container img");
+    var linkList = document.querySelector(".arrow-container .link-list");
+
+    arrow.addEventListener("click", function() {
+        if (linkList.style.display === "none") {
+            linkList.style.display = "block";
+        } else {
+            linkList.style.display = "none";
+        }
+    });
+});
+
+ScrollReveal().reveal('#fusee', {
+    origin: 'bottom', 
+    distance: '100px',
+    duration: 1000, 
+    delay: 200, 
+    easing: 'ease',
+    mobile: true,
+    reset: false 
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var cotationLink = document.querySelector(".link-list a:first-child");
+
+    cotationLink.addEventListener("click", function(event) {
+        event.preventDefault(); 
+        
+        var sectionCotation = document.getElementById("cotation");
+        var sectionCotationRect = sectionCotation.getBoundingClientRect();
+        var offsetTop = sectionCotationRect.top + window.pageYOffset;
+
+        window.scrollTo({
+            top: offsetTop,
+            behavior: "smooth" 
+        });
+    });
+});
+
+
+    document.addEventListener("DOMContentLoaded", function() {
+        var privilegesLink = document.querySelector(".link-list li:nth-child(2) a"); 
+
+        privilegesLink.addEventListener("click", function(event) {
+            event.preventDefault(); 
+            
+            var privilegesSection = document.getElementById("privileges");
+            var privilegesSectionRect = privilegesSection.getBoundingClientRect();
+            var offsetTop = privilegesSectionRect.top + window.pageYOffset;
+
+            window.scrollTo({
+                top: offsetTop,
+                behavior: "smooth"
+            });
+        });
+    });
+
+
+
+    
+
+
+    document.addEventListener("DOMContentLoaded", function () {
+        const carousel = document.getElementById("carousel");
+        const prevBtn = document.getElementById("prevBtn");
+        const nextBtn = document.getElementById("nextBtn");
+    
+        const images = [
+            "img/woman.jpg", "img/woman.jpg", "img/woman.jpg",
+            "img/01.jpg", "img/01.jpg", "img/01.jpg",
+            "img/woman2.jpg", "img/woman2.jpg", "img/woman2.jpg"
+        ];
+    
+        let currentIndex = 0;
+    
+        function updateCarousel() {
+            carousel.innerHTML = "";
+            for (let i = currentIndex; i < currentIndex + 3; i++) {
+                const index = i % images.length;
+                const item = createCarouselItem(images[index]);
+                carousel.appendChild(item);
+            }
+        }
+    
+        function createCarouselItem(imageSrc) {
+            const item = document.createElement("div");
+            item.classList.add("carousel-item");
+    
+            const img = document.createElement("img");
+            img.src = imageSrc;
+            item.appendChild(img);
+    
+            return item;
+        }
+    
+        updateCarousel();
+    
+        function nextSlide() {
+            currentIndex = (currentIndex + 3) % images.length;
+            updateCarousel();
+        }
+    
+        function prevSlide() {
+            currentIndex = (currentIndex - 3 + images.length) % images.length;
+            updateCarousel();
+        }
+    
+        nextBtn.addEventListener("click", nextSlide);
+        prevBtn.addEventListener("click", prevSlide);
+    });
+    
